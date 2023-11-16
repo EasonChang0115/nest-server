23f14941cf39804fa4581b8987a19b7b8813cb35
eason.ec.chang <eason.ec.chang@deltaww.com>
2023-11-16 11:23:21 +0800

    refactor: uwl65/3

--- src/socket/socket.gateway.ts
+++ src/socket/socket.gateway.ts
@@ -2,37 +2,41 @@ import {
   WebSocketGateway,
   SubscribeMessage,
   MessageBody,
+  WebSocketServer,
+  ConnectedSocket,
 } from '@nestjs/websockets';
 import { SocketService } from './socket.service';
-import { CreateSocketDto } from './dto/create-socket.dto';
-import { UpdateSocketDto } from './dto/update-socket.dto';
+import { Server, Socket } from 'socket.io';
 
-@WebSocketGateway()
+@WebSocketGateway(8080, {
+  allowEIO3: true, // 允许使用 EIO3
+  cors: {
+    origin: 'http://localhost:5500',
+    credentials: true,
+  },
+})
 export class SocketGateway {
+  @WebSocketServer()
+  server: Server;
   constructor(private readonly socketService: SocketService) {}
 
-  @SubscribeMessage('createSocket')
-  create(@MessageBody() createSocketDto: CreateSocketDto) {
-    return this.socketService.create(createSocketDto);
+  // 處理連線事件
+  handleConnection(client: any) {
+    console.log('client connected');
   }
 
-  @SubscribeMessage('findAllSocket')
-  findAll() {
-    return this.socketService.findAll();
+  // 處理斷線事件
+  handleDisconnect(client: any) {
+    // Handle disconnection event
+    console.log('client disconnected');
   }
 
-  @SubscribeMessage('findOneSocket')
-  findOne(@MessageBody() id: number) {
-    return this.socketService.findOne(id);
-  }
-
-  @SubscribeMessage('updateSocket')
-  update(@MessageBody() updateSocketDto: UpdateSocketDto) {
-    return this.socketService.update(updateSocketDto.id, updateSocketDto);
-  }
-
-  @SubscribeMessage('removeSocket')
-  remove(@MessageBody() id: number) {
-    return this.socketService.remove(id);
+  @SubscribeMessage('transcribeEvent')
+  listenClientAudioBuffer(
+    @MessageBody() body: any,
+    @ConnectedSocket() client: Socket,
+  ) {
+    client.emit('onTranscribe', body);
+    // this.server.emit('onTranscribe', body); // *
   }
 }
